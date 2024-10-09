7993587fc679e819c8e13f7eec8348c19feb3939
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2024-10-09 13:20:18 +0000

    notes updated

--- Notes.py
+++ Notes.py
@@ -1,1 +1,24 @@
+'''
+    Lesson: If statments
+    Author: Mr. Kalisz
+    Date Created: Oct 9, 2024
+    Date Last Modified: Oct 9, 2024
+'''
 
+#if statement
+
+num = int(input("Input an integer: "))
+
+#if <condition>:
+if num < 6:
+    #Indenting means this code is inside the if statement
+    #Code inside the if statement only runs when the condition is true
+    num = num + 7
+    print("num is less than 6")
+
+
+if num > 6:
+    print("num is greater than 6")
+
+print("This always runs")
+print(num)
