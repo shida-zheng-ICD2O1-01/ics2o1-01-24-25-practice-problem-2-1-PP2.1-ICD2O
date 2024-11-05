b6863fe76375de7420b45665079c3c5b5643757f
shida-zheng-ICD2O1-01 <shida.zheng@student.tdsb.on.ca>
2024-11-05 10:35:21 -0500

    Update PP2_1.py

--- PP2_1.py
+++ PP2_1.py
@@ -2,15 +2,25 @@
 
 def q1(): 
   #Write Assignment code here
+  num = int(input("Input a integer:"))
 
+  if num % 2 == 0:
+    print(f"{num} is even")
+  else:
+    print(f"{num} is odd")
 
 def q2(): 
   #Write Assignment code here
+  name = input("Input a name:")
+  if name == "Kalisz":
+    print("teacher")
+  else :
+    print("student")
 
 
 
 #Do not alter the following code
 #Comment out the following code when running your tests
 
-q1()
-q2()
+#q1()
+#q2()
